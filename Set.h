854857e92036f5b1dde44772f402d03269f5afc3
Stepan <stdnsv@gmail.com>
2022-03-02 13:01:45 +0400

    release the class Set methods

--- Set.h
+++ Set.h
@@ -1,5 +1,96 @@
 #pragma once
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+struct node {
+	int data;
+	node* left;
+	node* right;
+public:
+	void iprint(node* root)
+	{
+		if (root == NULL)   // Базовый случай
+		{
+			return;
+		}
+		iprint(root->left);   //рекурсивный вызов левого поддерева
+		cout << root->data << " ";
+		iprint(root->right);  //рекурсивный вызов правого поддерева
+	}
+};
 class Set
 {
+private:
+	node* root;
+public:
+	Set()
+	{
+		root = NULL;
+	}
+	bool find(int key)
+	{
+		if (!root) return false;
+		node* tmproot = root;
+		while (tmproot)
+		{
+			if (key > tmproot->data)
+			{
+				tmproot = tmproot->right;
+			}
+			else if (key < tmproot->data)
+			{
+				tmproot = tmproot->left;
+			}
+			else if (key == tmproot->data) return true;
+		}
+		return false;
+	}
+	bool insert(int key)
+	{	
+		if (!root)
+		{
+			root = new node;
+			root->data = key;
+			root->right = NULL;
+			root->left = NULL;
+			return true;
+		}
+		if (!find(key))
+		{
+			node* tmproot = root;
+			while (tmproot)
+			{
+				if (tmproot->data > key)
+					{	
+					if (tmproot->left == NULL) 
+					{
+						tmproot->left = new node; 
+						tmproot = tmproot->left;
+						break;
+					}
+						tmproot = tmproot->left;
+					}
+				else if (tmproot->data < key)
+					{
+					if (tmproot->right == NULL)
+					{
+						tmproot->right = new node;
+						tmproot = tmproot->right;
+						break;
+					}
+						tmproot = tmproot->right;
+					}
+			}
+			tmproot->data = key;
+			tmproot->right = NULL;
+			tmproot->left = NULL;
+			return true;
+		}
+		else return false;
+	}
+	void print()
+	{
+		root->iprint(root);
+	}
 };
 
--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,10 @@
+#include "Set.h"
 int main()
 {
+	Set s;
+	s.insert(2);
+	s.insert(30);
+	s.insert(3);
+	s.print();
 	return 0;
 }
